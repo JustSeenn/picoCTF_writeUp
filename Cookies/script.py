27ba8b5c6b1d521990337d472308a5689263e9e8
BOILLOT DEVALLIERE Vincent <63784031+JustSeenn@users.noreply.github.com>
2021-09-20 16:17:50 +0200

    Update script.py

--- Cookies/script.py
+++ Cookies/script.py
@@ -1,8 +1,7 @@
 import os
 
 cmpt = 1
-for i in range (0,25):
+for i in range (0,30):
         cmd = '''curl http://mercury.picoctf.net:29649/ -s -H "Cookie: name=''' + str(cmpt) + ''';" -L | grep -i PicoCTF{'''
         cmpt = cmpt+1
-        print(cmpt)
         os.system(cmd)
